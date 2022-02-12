fd2206d0851996f868878c54d4e121b79abcc990
burakJs <burakimdat13@gmail.com>
2022-02-12 17:12:07 +0300

    feat: added navigation service and route management

---
+++ lib/core/init/navigation/navigation_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'INavigation_service.dart';
+
+class NavigationService extends INavigationService {
+  static NavigationService? _instance;
+  static NavigationService get instance => _instance ??= NavigationService._init();
+  NavigationService._init();
+
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+  final removeAllOldRoutes = (Route<dynamic> route) => false;
+
+  @override
+  Future<void> navigateToPage(String path, {Object? data}) async {
+    await navigatorKey.currentState?.pushNamed(path, arguments: data ?? '');
+  }
+
+  @override
+  Future<void> navigateToPageClear(String path, {Object? data}) async {
+    await navigatorKey.currentState?.pushNamedAndRemoveUntil(path, removeAllOldRoutes, arguments: data ?? '');
+  }
+}
